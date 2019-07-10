91736308a87a9464d34902bb6c4d07407919547e
Denis <denshufly@mail.ru>
2019-07-10 16:55:26 +0600

    CarMVC

---
+++ WebApplicationCar/Data/WebApplicationCarContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplicationCar.Models
+{
+    public class WebApplicationCarContext : DbContext
+    {
+        public WebApplicationCarContext()
+        {
+        }
+
+        public WebApplicationCarContext (DbContextOptions<WebApplicationCarContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplicationCar.Models.Car> Cars { get; set; }
+    }
+}
---
+++ WebApplicationCar/Models/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationCar.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Titel { get; set; }
+        [DataType(DataType.Date)]
+        public  DateTime Date { get; set; }
+        public int Price { get; set; }
+
+    }
+}
